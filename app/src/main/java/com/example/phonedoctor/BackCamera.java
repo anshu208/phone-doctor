87ce32148df7a77798c247a8f87ae071eebcdf9f
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-04-19 02:54:27 +0530

    Camera Done

--- app/src/main/java/com/example/phonedoctor/BackCamera.java
+++ app/src/main/java/com/example/phonedoctor/BackCamera.java
@@ -1,10 +1,26 @@
 package com.example.phonedoctor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class BackCamera extends AppCompatActivity {
+
+    private Button checkCameraButton;
+    private Button openCameraButton;
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +29,60 @@ public class BackCamera extends AppCompatActivity {
 
         // setting action bar text
         getSupportActionBar().setTitle("Back Camera Test");
+
+        checkCameraButton = findViewById(R.id.button);
+        checkCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasCamera()) {
+                    Toast.makeText(BackCamera.this, "Camera available", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(BackCamera.this, "Camera not available", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        openCameraButton = findViewById(R.id.openCameraButton);
+        openCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(BackCamera.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    ActivityCompat.requestPermissions(BackCamera.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                }
+            }
+        });
+    }
+
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri photoUri = data.getData();
+            // Do something with the photo here
+        }
     }
 }
+
