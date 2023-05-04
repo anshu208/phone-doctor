22afaa29c157e45f37f87e4a98f84efcaecfca1b
Anshu Kumar <anshu.study20@gmail.com>
2023-05-04 12:16:54 +0530

    adding style

--- app/src/main/java/com/example/phonedoctor/MainActivity.java
+++ app/src/main/java/com/example/phonedoctor/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.phonedoctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.example.phonedoctor.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this , SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 
     //going from main activity to frontcamera activity
