76a26710d724a5838c5fb3d3c89a225ecd387e94
uk47kumar <uk47kumar@gmail.com>
2023-05-16 10:09:12 +0530

    blutooth

--- app/src/main/java/com/example/phonedoctor/Bluetooth.java
+++ app/src/main/java/com/example/phonedoctor/Bluetooth.java
@@ -1,10 +1,20 @@
 package com.example.phonedoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class Bluetooth extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,40 @@ public class Bluetooth extends AppCompatActivity {
 
         // setting action bar text
         getSupportActionBar().setTitle("Bluetooth Test");
+
+        // Get the default Bluetooth adapter
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Check if the device supports Bluetooth
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Check if Bluetooth is enabled on the device
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivityForResult(enableBluetoothIntent, 0);
+        }
+
+        // Get a list of paired devices
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                Toast.makeText(this, "Paired Device: " + device.getName() + " - " + device.getAddress(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "No paired devices found", Toast.LENGTH_SHORT).show();
+        }
     }
 }
