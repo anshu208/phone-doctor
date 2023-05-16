368e62293d517e2d8eddfa9fc0d478100a44e24b
Anshu Kumar <anshu.study20@gmail.com>
2023-05-16 12:07:28 +0530

    speaker

--- app/src/main/java/com/example/phonedoctor/Receiver.java
+++ app/src/main/java/com/example/phonedoctor/Receiver.java
@@ -1,17 +1,48 @@
 package com.example.phonedoctor;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Receiver extends AppCompatActivity {
+
+    private AudioManager audioManager;
+    private ToneGenerator toneGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
 
-        // setting action bar text
-        getSupportActionBar().setTitle("Receiver Test");
+        // Get the AudioManager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        // Create a ToneGenerator to play a beep sound
+        toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+
+        // Check if the speaker is working
+        if (audioManager.isSpeakerphoneOn()) {
+            // If the speaker is on, play a beep sound through the speaker
+            toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP2, 500);
+            Toast.makeText(this, "Speaker is not working", Toast.LENGTH_SHORT).show();
+        } else {
+            // If the speaker is off, turn it on and play a beep sound through the speaker
+            audioManager.setSpeakerphoneOn(true);
+            toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP2, 500);
+            audioManager.setSpeakerphoneOn(false);
+            Toast.makeText(this, "Speaker is  working", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Release the ToneGenerator
+        toneGenerator.release();
     }
 }
